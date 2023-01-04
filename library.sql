e83406c73ab78a73c21b4473e834f0ac559b3702
kim <91533432+kimberlymazel@users.noreply.github.com>
2023-01-04 15:40:58 +0700

    Update library.sql

--- library.sql
+++ library.sql
@@ -55,17 +55,6 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Table structure for table `author`
---
-
-/*
-CREATE TABLE `author` (
-  `authorId` int(10) NOT NULL,
-  `authorFname` varchar(20) NOT NULL,
-  `authorLname` varchar(20) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-*/
---
 -- Table structure for table `book`
 --
 
@@ -74,7 +63,7 @@ CREATE TABLE `book` (
   `title` varchar(100) NOT NULL,
   `author_name` varchar(50) NOT NULL,
   `publishId` varchar(10) NOT NULL,
-  `serialNo` varchar(20) NOT NULL,
+  `serialNo` int(13) NOT NULL, -- isbn
   `quantity` int(11) NOT NULL,
   `regisDate` date NOT NULL,
   `regisBy` varchar(50) NOT NULL,
@@ -119,7 +108,7 @@ CREATE TABLE `publisher` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userid`, `fname`, `lname`, `birthday`, `email`, `address`, 'regisdate') VALUES
+INSERT INTO `user` (`userId`, `userFname`, `userLname`, `birthday`, `email`, `address`, 'regisDate') VALUES
 (20000101, 'John', 'Smith', '2000-01-01', 'johnsmith@mail.com', 'Jl. Jenderal Sudirman', '2022-10-05');
 
 --
@@ -130,42 +119,35 @@ INSERT INTO `user` (`userid`, `fname`, `lname`, `birthday`, `email`, `address`, 
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`adminid`);
+  ADD PRIMARY KEY (`adminId`);
 
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`authorid`);
 
---
 -- Indexes for table `book`
 --
 ALTER TABLE `book`
-  ADD PRIMARY KEY (`bookid`),
-  ADD KEY `authorid` (`authorid`),
-  ADD KEY `publishid` (`publishid`);
+  ADD PRIMARY KEY (`bookId`),
+  ADD KEY `publishId` (`publishId`);
 
 --
 -- Indexes for table `book_borrowreturn`
 --
 ALTER TABLE `book_borrowreturn`
-  ADD PRIMARY KEY (`borrowid`),
-  ADD UNIQUE KEY `bookid` (`bookid`),
-  ADD KEY `userid` (`userid`,`adminid`),
-  ADD KEY `adminid` (`adminid`);
+  ADD PRIMARY KEY (`borrowId`),
+  ADD UNIQUE KEY `bookId` (`bookId`),
+  ADD KEY `userId` (`userId`,`adminId`),
+  ADD KEY `adminId` (`adminId`);
 
 --
 -- Indexes for table `publisher`
 --
 ALTER TABLE `publisher`
-  ADD PRIMARY KEY (`publishid`);
+  ADD PRIMARY KEY (`publishId`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`userid`);
+  ADD PRIMARY KEY (`userId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -175,54 +157,42 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `adminid` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `authorid` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `adminId` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `book_borrowreturn`
 --
 ALTER TABLE `book_borrowreturn`
-  MODIFY `borrowid` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `borrowId` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12346;
+  MODIFY `userId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12346;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `author`
---
-ALTER TABLE `author`
-  ADD CONSTRAINT `author_ibfk_1` FOREIGN KEY (`authorid`) REFERENCES `book` (`authorid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `book`
 --
 ALTER TABLE `book`
-  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `book_borrowreturn` (`bookid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book_borrowreturn` (`bookId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `book_borrowreturn`
 --
 ALTER TABLE `book_borrowreturn`
-  ADD CONSTRAINT `book_borrowreturn_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `book_borrowreturn_ibfk_2` FOREIGN KEY (`adminid`) REFERENCES `admin` (`adminid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `book_borrowreturn_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_borrowreturn_ibfk_2` FOREIGN KEY (`adminId`) REFERENCES `admin` (`adminId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `publisher`
 --
 ALTER TABLE `publisher`
-  ADD CONSTRAINT `publisher_ibfk_1` FOREIGN KEY (`publishid`) REFERENCES `book` (`publishid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `publisher_ibfk_1` FOREIGN KEY (`publishId`) REFERENCES `book` (`publishId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
